1252ecf7db711b610efb2282f3b19f1f8f342b5c
Zulrahman Asmurad <125336231+Zulrahman92@users.noreply.github.com>
2023-08-03 14:05:17 +0800

    add inline styling

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import HelloReact from "./components/HelloReact"
+
+function App() {
+  
+
+  return (
+    <>
+    <HelloReact />
+    <HelloReact />
+    <HelloReact />
+    <HelloReact />
+    <HelloReact />
+    <HelloReact />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HelloReact.jsx
@@ -0,0 +1,16 @@
+const HelloReact = () => {
+    return (
+        <p style={{
+            backgroundColor: "aliceblue",
+            color: "grey",
+            fontSize: 36,
+            padding: "24px",
+            borderRadius: 20,
+          }}
+          >
+            Hello React
+            </p>
+    );
+};
+
+export default HelloReact;
